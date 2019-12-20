78448dad9233445395e4831b8b5e5c27a209e6a6
Reid Beels <reid@thedyrt.com>
2019-12-19 17:17:52 -0800

    Ensure start_column comes before end_column (#84)
    On rare occasions (like the TrailingEmptyLines cop checking for newlines at the end of a file),
    rubocop can generate a start column that's larger than the end column and also report both of these as being on the same line.

--- lib/report_adapter.rb
+++ lib/report_adapter.rb
@@ -21,7 +21,7 @@ class ReportAdapter
       "#{total_offenses(report)} offense(s) found."
     end
 
-    def annotations(report) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def annotations(report) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
       annotation_list = []
       count = 0
       report['files'].each do |file|
@@ -31,6 +31,10 @@ class ReportAdapter
 
           location = offense['location']
           same_line = location['start_line'] == location['last_line']
+          has_columns = location['start_column'] && location['end_column']
+          if same_line && has_columns && location['start_column'] < location['end_column']
+            location['start_column'], location['end_column'] = location['end_column'], location['start_column']
+          end
           annotation_list.push(
             {
               'path': file['path'],
